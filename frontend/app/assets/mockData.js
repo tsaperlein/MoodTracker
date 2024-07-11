be1f496be715a9b19a8a63a1a194e5895367b1f4
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-07-11 16:11:25 +0300

    Create mock data for project

---
+++ frontend/app/assets/mockData.js
@@ -0,0 +1,22 @@
+export const surveyAnswers = {
+  1: {
+    emoji: 5,
+    keywords: ['happy', 'down'],
+    comment: 'Had a great day at work!',
+  },
+  2: {
+    emoji: 2,
+    keywords: ['excited'],
+    comment: 'Feeling a bit down today.',
+  },
+  3: {
+    emoji: 4,
+    keywords: ['amused', 'funny'],
+    comment: 'Completed a lot of tasks!',
+  },
+  4: {
+    emoji: 3,
+    keywords: ['happy', 'down'],
+    comment: 'It was an average day.',
+  },
+};
---
+++ frontend/app/assets/sampleData.js
@@ -0,0 +1,35 @@
+// sampleData.js
+
+export default sampleData = [
+  {
+    date: '2024/06/20',
+    hour: '10:00 AM',
+    diaryImage: 'https://via.placeholder.com/150/92c952',
+    text: 'Today I went to the park and enjoyed a sunny day.',
+  },
+  {
+    date: '2024/06/17',
+    hour: '11:00 AM',
+    diaryImage: 'https://via.placeholder.com/150/771796',
+    text: 'Had a great time with friends at the beach.',
+  },
+  {
+    date: '2024/06/16',
+    hour: '09:00 AM',
+    diaryImage: 'https://via.placeholder.com/150/24f355',
+    text: 'Started my day with a refreshing jog.',
+  },
+  {
+    date: '2024/06/15',
+    hour: '08:00 AM',
+    diaryImage: 'https://via.placeholder.com/150/d32776',
+    text: 'Enjoyed a cup of coffee at my favorite cafe.',
+  },
+  {
+    date: '2024/06/14',
+    hour: '07:00 AM',
+    diaryImage: 'https://via.placeholder.com/150/f66b97',
+    text: 'Went for a hike in the mountains.',
+  },
+  // Add more diary entries as needed
+];
