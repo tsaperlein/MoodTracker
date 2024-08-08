e3c4b3ce9891ff8034ccc85811f3934dd9461c78
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-08-08 14:32:38 +0300

    Set the database for the api endpoints

--- api/src/index.mjs
+++ api/src/index.mjs
@@ -1,55 +1,49 @@
 import express from "express";
 import dotenv from "dotenv";
-import { getXataClient } from "./xata.js";
-import {
-  User,
-  Emoji,
-  Quote,
-  Assigns,
-  DailyMood,
-  DiaryPage,
-  Survey,
-} from "./mt_database.js"; // assuming the data is in a separate file called data.js
+
+// Routes
+import initRoutes from "./routes/init.mjs";
+import authRoutes from "./routes/auth.mjs";
+import userRoutes from "./routes/user.mjs";
+import surveyRoutes from "./routes/survey.mjs";
+import answerRoutes from "./routes/answer.mjs";
+import questionRoutes from "./routes/question.mjs";
+import welcomeMoodRoutes from "./routes/welcomeMood.mjs";
+import quoteRoutes from "./routes/quote.mjs";
 
 dotenv.config();
 
 const { PORT } = process.env;
 
 const app = express();
-
 app.use(express.json({ limit: "50mb" }));
 
-const client = getXataClient();
+// Initialization
+app.use(initRoutes);
 
-async function createIfNotExists(collection, data) {
-  for (const item of data) {
-    const existingItem = await client.db[collection].read(item.id);
-    if (!existingItem) {
-      await client.db[collection].create(item);
-    }
-  }
-}
+// Authentication
+app.use(authRoutes);
 
-app.get("/init", async (req, res) => {
-  try {
-    await createIfNotExists("User", User);
-    await createIfNotExists("Emoji", Emoji);
-    await createIfNotExists("Quote", Quote);
-    await createIfNotExists("Assigns", Assigns);
-    await createIfNotExists("DailyMood", DailyMood);
-    await createIfNotExists("DiaryPage", DiaryPage);
-    await createIfNotExists("Survey", Survey);
+// User
+// app.use(userRoutes);
 
-    return res.json({ message: "Data initialized successfully" });
-  } catch (error) {
-    console.error("Error initializing data:", error);
-    return res.status(500).json({ error: "Failed to initialize data" });
-  }
-});
+// Survey
+app.use(surveyRoutes);
+
+// Answer
+app.use(answerRoutes);
+
+// Question
+app.use(questionRoutes);
+
+// Welcome Mood
+// app.use(welcomeMoodRoutes);
+
+// Quote
+// app.use(quoteRoutes);
 
 app.get("/", async (req, res) => {
-  const result = await client.db.User.getAll();
-  return res.json({ results: result });
+  return res.json({ ok: true });
 });
 
 app.listen(PORT, () => {
