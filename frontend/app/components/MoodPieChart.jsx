97ac0dfee804a767ee79d41955fc76e34774086c
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-08-24 20:14:28 +0300

    Finalize MoodPieChart

--- frontend/app/components/MoodPieChart.jsx
+++ frontend/app/components/MoodPieChart.jsx
@@ -1,37 +1,55 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+
+// Colors
+import colors from '../config/colors';
+// Fonts
+import fonts from '../config/fonts';
+
+// Mood Configuration
+import { moodConfig } from '../config/moodConfig';
+
+// Components
 import { PieChart } from 'react-native-gifted-charts';
 
-import colors from '../config/colors';
-import moodConfig from '../config/moodConfig';
+// Utilities
+import { getRatioOutcome } from '../utils/ratioUtil';
 
-export default function MoodPieChart({ data, chartColor = colors.blue800, size = 95 }) {
+// Window Height
+const height = Dimensions.get('window').height;
+
+export default function MoodPieChart({ data, chartColor = colors.blue800, size = height / 9 }) {
   const [selectedMood, setSelectedMood] = useState(null);
 
-  const totalMoods = data.length;
-
   // Calculate the frequency of each mood
-  const moodCounts = data.reduce((acc, moodData) => {
-    const mood = Object.keys(moodConfig).find((key) => moodConfig[key].value === moodData.value);
-    if (!acc[mood]) {
-      acc[mood] = 0;
-    }
-    acc[mood] += 1;
-    return acc;
-  }, {});
+  const moodCounts = {
+    awful: 0,
+    sad: 0,
+    neutral: 0,
+    good: 0,
+    happy: 0,
+  };
 
-  // Calculate the percentage for each mood and create the pie data
-  const pieData = Object.keys(moodCounts).map((mood) => {
-    const percentage = ((moodCounts[mood] / totalMoods) * 100).toFixed(2);
-    return {
-      value: parseFloat(percentage),
-      color: moodConfig[mood].backgroundColor,
-      gradientCenterColor: moodConfig[mood].color,
-      mood: mood,
-      focused: selectedMood === mood,
-      onPress: () => handleSectionPress(mood),
-    };
+  data.forEach((item) => {
+    const mood = getRatioOutcome(item.value, 26, 'emotion');
+    moodCounts[mood] += 1;
   });
+
+  // Convert moodCounts into pie chart data
+  const totalMoods = data.length;
+  const pieData = Object.keys(moodCounts)
+    .filter((mood) => moodCounts[mood] > 0)
+    .map((mood) => {
+      const percentage = ((moodCounts[mood] / totalMoods) * 100).toFixed(2);
+      return {
+        value: parseFloat(percentage),
+        color: moodConfig[mood].calendar.backgroundColor,
+        gradientCenterColor: moodConfig[mood].calendar.color,
+        mood: mood,
+        focused: selectedMood === mood,
+        onPress: () => handleSectionPress(mood),
+      };
+    });
 
   const handleSectionPress = (mood) => {
     setSelectedMood(mood === selectedMood ? null : mood);
@@ -42,9 +60,9 @@ export default function MoodPieChart({ data, chartColor = colors.blue800, size =
       <PieChart
         data={pieData}
         donut
-        strokeWidth={2}
+        strokeWidth={3}
         strokeColor={chartColor}
-        innerCircleBorderWidth={2}
+        innerCircleBorderWidth={3}
         innerCircleBorderColor={chartColor}
         radius={size}
         innerRadius={size - 40}
@@ -54,9 +72,11 @@ export default function MoodPieChart({ data, chartColor = colors.blue800, size =
           if (selectedMood) {
             const selectedData = pieData.find((data) => data.mood === selectedMood);
             return (
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <View style={styles.centerLabelContainer}>
                 <Text style={styles.centerLabel}>{selectedData.value}%</Text>
-                <Text style={styles.centerLabel}>{selectedMood}</Text>
+                <Text style={styles.centerLabel}>
+                  {selectedMood.charAt(0).toUpperCase() + selectedMood.slice(1)}
+                </Text>
               </View>
             );
           }
@@ -73,5 +93,14 @@ const styles = StyleSheet.create({
     padding: '2%',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  centerLabelContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerLabel: {
+    fontSize: 18,
+    color: colors.blue200,
+    fontFamily: fonts.medium,
   },
 });
