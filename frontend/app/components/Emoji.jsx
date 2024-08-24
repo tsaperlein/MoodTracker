a4bd08f15a45dad5646d35f03bb544ef9eef7a15
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-08-24 19:07:59 +0300

    Finalize EmojiSelector and Emoji

---
+++ frontend/app/components/Emoji.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, Pressable, Animated } from 'react-native';
+
+// Colors
+import colors from '../config/colors';
+// Icons
+import { FontAwesome6 } from '@expo/vector-icons';
+
+// Mood Configuration
+import { moodConfig } from '../config/moodConfig';
+
+export default function Emoji({
+  mood,
+  selected,
+  onPress,
+  disabled,
+  emojiColor = colors.blue100,
+  emojiBackgroundColor = colors.blue100,
+}) {
+  const [scale] = useState(new Animated.Value(1));
+
+  const handlePress = () => {
+    if (disabled) return;
+    Animated.sequence([
+      Animated.timing(scale, {
+        toValue: 1.2,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(scale, {
+        toValue: 1,
+        duration: 100,
+        useNativeDriver: true,
+      }),
+    ]).start();
+
+    onPress();
+  };
+
+  const color = selected ? moodConfig[mood].general.color : emojiColor;
+  const backgroundColor = selected ? emojiBackgroundColor : 'transparent';
+
+  return (
+    <View style={styles.container}>
+      <Pressable onPress={handlePress}>
+        <Animated.View style={[styles.iconContainer, { backgroundColor, transform: [{ scale }] }]}>
+          <FontAwesome6 name={moodConfig[mood].icon} size={40} color={color} />
+        </Animated.View>
+      </Pressable>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  iconContainer: {
+    padding: '12%',
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
--- frontend/app/components/EmojiSelector.jsx
+++ frontend/app/components/EmojiSelector.jsx
@@ -1,57 +1,25 @@
-// EmojiSelector.js
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Pressable, Animated } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 // Colors
 import colors from '../config/colors';
-// Icons
-import { FontAwesome6 } from '@expo/vector-icons';
+// Fonts
+import fonts from '../config/fonts';
 
-const moods = ['awful', 'sad', 'neutral', 'good', 'happy'];
-const moodConfig = {
-  awful: { icon: 'sad-cry', color: 'darkred' },
-  sad: { icon: 'frown', color: 'red' },
-  neutral: { icon: 'meh', color: 'orange' },
-  good: { icon: 'smile', color: 'blue' },
-  happy: { icon: 'laugh-beam', color: 'green' },
-};
+// Components
+import Emoji from './Emoji';
 
-function Emoji({ mood, selected, onPress, disabled }) {
-  const [scale] = useState(new Animated.Value(1));
+// Mood Configuration
+import { moods } from '../config/moodConfig';
 
-  const handlePress = () => {
-    if (disabled) return;
-    Animated.sequence([
-      Animated.timing(scale, {
-        toValue: 1.2,
-        duration: 100,
-        useNativeDriver: true,
-      }),
-      Animated.timing(scale, {
-        toValue: 1,
-        duration: 100,
-        useNativeDriver: true,
-      }),
-    ]).start();
-
-    onPress(); // Call the parent handler
-  };
-
-  const backgroundColor = selected ? colors.white : 'transparent';
-  const iconColor = selected ? moodConfig[mood].color : colors.blue400;
-
-  return (
-    <View style={styles.container}>
-      <Pressable onPress={handlePress}>
-        <Animated.View style={[styles.iconContainer, { backgroundColor, transform: [{ scale }] }]}>
-          <FontAwesome6 name={moodConfig[mood].icon} size={40} color={iconColor} />
-        </Animated.View>
-      </Pressable>
-    </View>
-  );
-}
-
-export default function EmojiSelector({ setEmojiSelected, initialMood, disabled }) {
+export default function EmojiSelector({
+  setEmojiSelected,
+  initialMood,
+  disabled,
+  emojiColor,
+  emojiBackgroundColor,
+  underTextColor,
+}) {
   const [selectedMood, setSelectedMood] = useState(initialMood);
 
   useEffect(() => {
@@ -71,33 +39,46 @@ export default function EmojiSelector({ setEmojiSelected, initialMood, disabled 
 
   return (
     <View style={styles.container}>
-      {moods.map((mood) => (
-        <Emoji
-          key={mood}
-          mood={mood}
-          selected={selectedMood === mood}
-          onPress={() => handlePress(mood)}
-          disabled={disabled}
-        />
-      ))}
+      <View style={styles.moodContainer}>
+        {moods.map((mood) => (
+          <Emoji
+            key={mood}
+            mood={mood}
+            selected={selectedMood === mood}
+            onPress={() => handlePress(mood)}
+            disabled={disabled}
+            emojiColor={emojiColor}
+            emojiBackgroundColor={emojiBackgroundColor}
+          />
+        ))}
+      </View>
+      <View style={styles.underTextContainer}>
+        <Text style={[styles.underText, { color: underTextColor }]}>Choose one of the </Text>
+        <Text
+          style={[styles.underText, { color: underTextColor, textDecorationLine: 'underline' }]}
+        >
+          Welcome Moods
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  moodContainer: {
+    flex: 5,
+    width: '100%',
+    flexDirection: 'row',
+  },
+  underTextContainer: {
     flex: 1,
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
-  emojiContainer: {
-    flex: 1 / 5,
-  },
-  iconContainer: {
-    padding: 10,
-    borderRadius: 50,
-    alignItems: 'center',
+    width: '100%',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  underText: {
+    fontSize: 18,
+    fontFamily: fonts.italicBold,
   },
 });
