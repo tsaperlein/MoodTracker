7b575026f13e7e0aab9d9f8686780f1fc4065642
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-08-24 13:39:36 +0300

    Finalize mood configuration file

--- frontend/app/config/moodConfig.js
+++ frontend/app/config/moodConfig.js
@@ -1,17 +1,74 @@
 import colors from './colors';
 
+const moods = ['awful', 'sad', 'neutral', 'good', 'happy'];
+
 const moodConfig = {
   nothing: {
-    value: 0,
     icon: 'face-retouching-off',
-    color: colors.gray500,
-    backgroundColor: colors.gray800,
+    general: {
+      color: colors.gray600,
+      backgroundColor: colors.gray800,
+    },
+    calendar: {
+      color: colors.gray600,
+      backgroundColor: colors.gray800,
+    },
   },
-  awful: { value: 1, icon: 'sad-cry', color: colors.red500, backgroundColor: colors.red300 },
-  sad: { value: 2, icon: 'frown', color: colors.yellow500, backgroundColor: colors.yellow300 },
-  neutral: { value: 3, icon: 'meh', color: colors.yellow600, backgroundColor: colors.yellow400 },
-  good: { value: 4, icon: 'smile', color: colors.green500, backgroundColor: colors.green300 },
-  happy: { value: 5, icon: 'laugh-beam', color: colors.blue500, backgroundColor: colors.blue300 },
+  awful: {
+    icon: 'sad-cry',
+    general: {
+      color: colors.red600,
+      backgroundColor: colors.red300,
+    },
+    calendar: {
+      color: colors.red800,
+      backgroundColor: colors.red500,
+    },
+  },
+  sad: {
+    icon: 'frown',
+    general: {
+      color: colors.orange600,
+      backgroundColor: colors.orange300,
+    },
+    calendar: {
+      color: colors.orange800,
+      backgroundColor: colors.orange500,
+    },
+  },
+  neutral: {
+    icon: 'meh',
+    general: {
+      color: colors.yellow600,
+      backgroundColor: colors.yellow300,
+    },
+    calendar: {
+      color: colors.yellow800,
+      backgroundColor: colors.yellow500,
+    },
+  },
+  good: {
+    icon: 'smile',
+    general: {
+      color: colors.green600,
+      backgroundColor: colors.green300,
+    },
+    calendar: {
+      color: colors.green800,
+      backgroundColor: colors.green500,
+    },
+  },
+  happy: {
+    icon: 'laugh-beam',
+    general: {
+      color: colors.blue600,
+      backgroundColor: colors.blue300,
+    },
+    calendar: {
+      color: colors.blue800,
+      backgroundColor: colors.blue500,
+    },
+  },
 };
 
-export default moodConfig;
+export { moods, moodConfig };
