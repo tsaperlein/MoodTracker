365e52b63c567794b2ec52b2bccbb3c40463f0ca
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-08-24 13:59:16 +0300

    Finalize DailyMood

--- frontend/app/components/DailyMood.jsx
+++ frontend/app/components/DailyMood.jsx
@@ -1,37 +1,46 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import React from 'react';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
+// Fonts
+import fonts from '../config/fonts';
 // Icons
-import { FontAwesome6 } from '@expo/vector-icons';
-// Mood Config
-import moodConfig from '../config/moodConfig';
+import { FontAwesome6, MaterialIcons } from '@expo/vector-icons';
+
+// Mood Configuration
+import { moodConfig } from '../config/moodConfig';
 
 // Window width
 const width = Dimensions.get('window').width;
 
-export default function DailyMood({ mood = 'neutral' }) {
-  const { icon, color, backgroundColor } = moodConfig[mood];
+export default function DailyMood({ mood = 'nothing', datetime = null }) {
+  if (mood === 'additional')
+    return <View style={[styles.container, { backgroundColor: 'transparent' }]}></View>;
 
-  return mood === 'nothing' ? (
-    <View style={[styles.container, { backgroundColor: 'transparent' }]}></View>
-  ) : (
+  const {
+    icon,
+    general: { color, backgroundColor },
+  } = moodConfig[mood];
+
+  // Convert the datetime to a Date object
+  const date = datetime ? new Date(datetime) : new Date();
+
+  // Get the day of the week and day of the month
+  const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'short' });
+  const dayOfMonth = date.getDate();
+
+  return (
     <View style={[styles.container, { backgroundColor }]}>
       <View style={styles.labelContainer}>
-        <Text style={styles.labelStyle}>16</Text>
-        <Text style={styles.labelStyle}>Mon</Text>
+        <Text style={styles.labelStyle}>{dayOfMonth}</Text>
+        <Text style={styles.labelStyle}>{dayOfWeek}</Text>
       </View>
-      <View style={{ flex: 1 / 3, justifyContent: 'center', alignItems: 'center' }}>
-        <View
-          style={{
-            height: '90%',
-            width: '70%',
-            backgroundColor: color,
-            borderRadius: 10,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <FontAwesome6 name={icon} size={24} color={backgroundColor} />
+      <View style={styles.iconSection}>
+        <View style={[styles.iconContainer, { backgroundColor: color }]}>
+          {mood === 'nothing' ? (
+            <MaterialIcons name={icon} size={24} color={backgroundColor} />
+          ) : (
+            <FontAwesome6 name={icon} size={24} color={backgroundColor} />
+          )}
         </View>
       </View>
     </View>
@@ -54,7 +63,19 @@ const styles = StyleSheet.create({
   },
   labelStyle: {
     fontSize: 16,
-    fontFamily: 'outfitMedium',
+    fontFamily: fonts.medium,
     margin: '8%',
+  },
+  iconSection: {
+    flex: 1 / 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconContainer: {
+    height: '90%',
+    width: '70%',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
