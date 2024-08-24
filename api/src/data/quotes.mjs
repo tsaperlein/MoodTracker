a697ec3cd3748de48d230740e3ab23e693452817
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-08-24 13:26:43 +0300

    Create data for database

---
+++ api/src/data/quotes.mjs
@@ -0,0 +1,78 @@
+// quotesData.js
+export const quotes = [
+  {
+    author: "Dalai Lama",
+    level: "good",
+    text: "Happiness is not something ready-made. It comes from your own actions.",
+  },
+  {
+    author: "Maya Angelou",
+    level: "good",
+    text: "You may not control all the events that happen to you, but you can decide not to be reduced by them.",
+  },
+  {
+    author: "Ralph Waldo Emerson",
+    level: "good",
+    text: "The only person you are destined to become is the person you decide to be.",
+  },
+  {
+    author: "Albert Einstein",
+    level: "good",
+    text: "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+  },
+  {
+    author: "Helen Keller",
+    level: "good",
+    text: "Keep your face to the sunshine and you cannot see a shadow.",
+  },
+  {
+    author: "Winston Churchill",
+    level: "mid",
+    text: "If you're going through hell, keep going.",
+  },
+  {
+    author: "Nelson Mandela",
+    level: "mid",
+    text: "Do not judge me by my successes, judge me by how many times I fell down and got back up again.",
+  },
+  {
+    author: "Henry Ford",
+    level: "mid",
+    text: "When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
+  },
+  {
+    author: "Confucius",
+    level: "mid",
+    text: "It does not matter how slowly you go as long as you do not stop.",
+  },
+  {
+    author: "Friedrich Nietzsche",
+    level: "bad",
+    text: "That which does not kill us makes us stronger.",
+  },
+  {
+    author: "J.K. Rowling",
+    level: "bad",
+    text: "Rock bottom became the solid foundation on which I rebuilt my life.",
+  },
+  {
+    author: "Charles Bukowski",
+    level: "bad",
+    text: "What matters most is how well you walk through the fire.",
+  },
+  {
+    author: "Albert Camus",
+    level: "bad",
+    text: "In the midst of winter, I found there was, within me, an invincible summer.",
+  },
+  {
+    author: "Victor Hugo",
+    level: "bad",
+    text: "Even the darkest night will end and the sun will rise.",
+  },
+  {
+    author: "Søren Kierkegaard",
+    level: "bad",
+    text: "Life can only be understood backwards; but it must be lived forwards.",
+  },
+];
