cf68eb90fc9c7f2c9fe0df7aaf54ced42d5800d2
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-09-02 21:13:13 +0300

    Add Press Start 2P font and comments

--- frontend/app/controllers/appController.js
+++ frontend/app/controllers/appController.js
@@ -1,21 +1,33 @@
 import { useState, useEffect, useContext } from 'react';
+
+// Font
 import * as Font from 'expo-font';
+
+// SplashScreen
 import * as SplashScreen from 'expo-splash-screen';
+
+// Authorization Services
 import { AuthContext } from 'context/AuthContext';
+// Mood Services
 import { MoodContext } from 'context/MoodContext';
 
 const getFonts = () => {
   return Font.loadAsync({
+    // Outfit
     outfit: require('../assets/fonts/Outfit-Regular.ttf'),
     outfitMedium: require('../assets/fonts/Outfit-Medium.ttf'),
     outfitBold: require('../assets/fonts/Outfit-Bold.ttf'),
+    // Roboto
     roboto: require('../assets/fonts/Roboto-Regular.ttf'),
     robotoItalic: require('../assets/fonts/Roboto-Italic.ttf'),
     robotoMedium: require('../assets/fonts/Roboto-Medium.ttf'),
     robotoMediumItalic: require('../assets/fonts/Roboto-MediumItalic.ttf'),
     robotoBold: require('../assets/fonts/Roboto-Bold.ttf'),
     robotoBoldItalic: require('../assets/fonts/Roboto-BoldItalic.ttf'),
+    // FjallaOne
     fjallaOne: require('../assets/fonts/FjallaOne-Regular.ttf'),
+    // Press Start 2P
+    pressStart2p: require('../assets/fonts/PressStart2P-Regular.ttf'),
   });
 };
 
