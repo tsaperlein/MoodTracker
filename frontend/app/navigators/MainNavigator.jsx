a2415bf57ccd6b693c8ec9792b9d0281f3f1142b
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-09-02 21:14:44 +0300

    Complete Main Navigator

--- frontend/app/navigators/MainNavigator.jsx
+++ frontend/app/navigators/MainNavigator.jsx
@@ -1,93 +1,57 @@
-import React from 'react';
-import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, StyleSheet, Text, Dimensions, TouchableOpacity } from 'react-native';
+
+// Colors
+import colors from '../constants/colors';
+// Fonts
+import fonts from '../constants/fonts';
+// Icons
+import { MaterialIcons } from '@expo/vector-icons';
 
 // Screens
 import Profile from '../screens/main/Profile';
+
+// Navigators
 import BottomNavigator from './BottomNavigator';
 import SurveyNavigator from './SurveyNavigator';
 import StatsNavigator from './StatsNavigator';
 
-// Colors
-import colors from '../config/colors';
-// Icons
-import { MaterialIcons, MaterialCommunityIcons, FontAwesome6 } from '@expo/vector-icons';
-
 // Components
-import { Avatar } from 'react-native-elements';
+import HeaderAvatar from '../components/HeaderAvatar';
+import HeaderTitle from '../components/HeaderTitle';
 
 // Navigation
-import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
-import { useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
+// Profile Services
+import { useProfile } from 'context/ProfileContext';
+
 // Header Height
-const headerHEIGHT = (Dimensions.get('window').height * 12) / 100;
-
-const HeaderAvatar = () => {
-  const navigation = useNavigation();
-  return (
-    <Avatar
-      rounded
-      source={require('../assets/images/tsaperlein.png')}
-      onPress={() => navigation.navigate('Profile')}
-      activeOpacity={0.5}
-      containerStyle={styles.avatarContainer}
-    />
-  );
-};
-
-const HeaderTitle = () => (
-  <View style={styles.headerTitleContainer}>
-    <Text style={styles.appName}>Hello, Alexandros</Text>
-    <View style={styles.featureContainer}>
-      <View style={[styles.feature, { backgroundColor: colors.blue700 }]}>
-        <MaterialCommunityIcons name="speedometer" size={20} color="white" />
-        <Text style={styles.featureText}>80% Score</Text>
-      </View>
-      <View style={[styles.feature, { backgroundColor: colors.blue100 }]}>
-        <FontAwesome6 name="calendar-check" size={20} color="black" />
-      </View>
-    </View>
-  </View>
-);
+import { headerHEIGHT } from '../constants/dimensions';
 
 const headerOptions = {
-  headerRight: () => <HeaderAvatar />,
   headerTitle: () => <HeaderTitle />,
+  headerRight: () => <HeaderAvatar />,
   headerTransparent: true,
+  headerTitleAlign: 'center',
   headerStyle: {
     height: headerHEIGHT,
     borderBottomWidth: 0,
   },
   headerBackTitleVisible: false,
-};
-
-const ProfileHeaderOptions = {
-  headerTitle: () => (
-    <View style={styles.profileHeaderTitleContainer}>
-      <Text style={styles.profileHeaderText}>Profile</Text>
-    </View>
-  ),
-  headerTransparent: true,
-  headerStyle: {
-    height: headerHEIGHT,
-    borderBottomWidth: 0,
-  },
-  headerBackTitleVisible: false,
-  headerBackImage: () => <MaterialIcons name="arrow-back-ios-new" size={30} color="white" />,
 };
 
 const StatsNavigatorOptions = {
   headerTitle: 'Statistics',
-  headerTransparent: false,
+  headerTitleAlign: 'center',
+  headerTransparent: true,
   headerStyle: {
     height: headerHEIGHT,
-    borderBottomWidth: 0,
-    backgroundColor: colors.blue900,
   },
   headerTitleStyle: {
     color: colors.blue300,
-    fontFamily: 'outfitBold',
+    fontFamily: fonts.bold,
     fontSize: 24,
   },
   headerBackTitleVisible: false,
@@ -97,80 +61,65 @@ const StatsNavigatorOptions = {
 };
 
 export default function MainNavigator() {
+  const { isChanged, handleSaveChanges } = useProfile();
+
+  const ProfileHeaderOptions = {
+    headerTitle: 'Profile',
+    headerTitleAlign: 'center',
+    headerTransparent: true,
+    headerStyle: {
+      height: headerHEIGHT,
+    },
+    headerTitleStyle: {
+      color: colors.blue300,
+      fontFamily: fonts.bold,
+      fontSize: 24,
+    },
+    headerBackTitleVisible: false,
+    headerBackImage: () => (
+      <MaterialIcons name="arrow-back-ios-new" size={30} color={colors.blue300} />
+    ),
+    headerRight: () => (
+      <TouchableOpacity onPress={handleSaveChanges.current} disabled={!isChanged}>
+        <Text style={[styles.doneButton, !isChanged && styles.disabledButton]}>Done</Text>
+      </TouchableOpacity>
+    ),
+  };
+
   return (
-    <Stack.Navigator
-      initialRouteName="Bottom Navigator"
-      screenOptions={{
-        gestureEnabled: true,
-        ...TransitionPresets.SlideFromRightIOS,
-      }}
-    >
-      <Stack.Screen name="Bottom Navigator" component={BottomNavigator} options={headerOptions} />
-      <Stack.Screen
-        name="Today's Survey"
-        component={SurveyNavigator}
-        options={{ headerShown: false }}
-        initialParams={{ mode: 'current' }}
-      />
-      <Stack.Screen
-        name="Stats Navigator"
-        component={StatsNavigator}
-        options={StatsNavigatorOptions}
-      />
-      <Stack.Screen name="Profile" component={Profile} options={ProfileHeaderOptions} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <Stack.Navigator initialRouteName="Bottom Navigator">
+        <Stack.Screen name="Bottom Navigator" component={BottomNavigator} options={headerOptions} />
+        <Stack.Screen
+          name="Survey Navigator"
+          component={SurveyNavigator}
+          options={{ headerShown: false }}
+          initialParams={{ mode: 'current' }}
+        />
+        <Stack.Screen
+          name="Stats Navigator"
+          component={StatsNavigator}
+          options={StatsNavigatorOptions}
+        />
+        <Stack.Screen name="Profile" component={Profile} options={ProfileHeaderOptions} />
+      </Stack.Navigator>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  avatarContainer: {
-    flex: 1,
-    width: '65%',
-    marginRight: '10%',
-    borderRadius: 50,
-    overflow: 'hidden',
-  },
-  headerTitleContainer: {
+  container: {
     flex: 1,
     width: '100%',
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.blue900,
   },
-  appName: {
-    flex: 1 / 2,
-    color: colors.blue800,
-    fontSize: 20,
-    fontWeight: '700',
-    textAlign: 'center',
+  doneButton: {
+    color: colors.blue400,
+    fontSize: 18,
+    fontFamily: fonts.medium,
+    marginRight: '15%',
   },
-  featureContainer: {
-    flex: 1 / 2,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  feature: {
-    flexDirection: 'row',
-    height: '100%',
-    margin: '2%',
-    paddingHorizontal: '3%',
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  featureText: {
-    color: 'white',
-    fontSize: 13,
-    fontFamily: 'outfitMedium',
-    marginLeft: '3%',
-  },
-  profileHeaderTitleContainer: {
-    justifyContent: 'center',
-  },
-  profileHeaderText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: '700',
+  disabledButton: {
+    color: colors.blue900,
   },
 });
