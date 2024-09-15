6c1e80854c466e8977143c9626f3d8dd1ac2d109
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-09-15 13:35:25 +0300

    Update Daily Survey Context for logging errors

--- frontend/app/context/DailySurveyContext.js
+++ frontend/app/context/DailySurveyContext.js
@@ -1,12 +1,10 @@
 import React, { createContext, useState, useEffect, useContext } from 'react';
+import { View, Text } from 'react-native';
 
 // Survey Services
 import { fetchLatestSurvey } from 'services/survey';
 // Answer Services
 import { isSurveyFinished } from 'services/answer';
-
-export const DailySurveyContext = createContext();
-export const useDailySurvey = () => useContext(DailySurveyContext);
 
 // Authorization Services
 import { useAuth } from 'context/AuthContext';
@@ -14,53 +12,59 @@ import { useAuth } from 'context/AuthContext';
 // Utilities
 import { adjustToGreeceTime, areSameDay } from '../utilities/datetime';
 
+export const DailySurveyContext = createContext();
+export const useDailySurvey = () => useContext(DailySurveyContext);
+
 export const DailySurveyProvider = ({ children }) => {
   const { authData } = useAuth();
+  const [loading, setLoading] = useState(true);
   const [dailySurveyCompleted, setDailySurveyCompleted] = useState(false);
 
   // Function to check the survey status
   const checkDailySurvey = async () => {
     if (!authData || !authData.id) {
+      console.log('No authData or ID found');
       setDailySurveyCompleted(false);
+      setLoading(false);
       return;
     }
     try {
       const latestSurvey = await fetchLatestSurvey(authData.id);
 
       if (!latestSurvey || !latestSurvey.id) {
+        console.log('No latest survey found');
         setDailySurveyCompleted(false);
+        setLoading(false);
         return;
       }
 
       const { finished } = await isSurveyFinished(latestSurvey.id);
 
-      console.log(finished);
-
       if (typeof finished !== 'boolean' || !finished) {
+        console.log('Survey not finished');
         setDailySurveyCompleted(false);
+        setLoading(false);
         return;
       }
 
-      // Ensure completionTime exists and is a valid Date
       if (!latestSurvey.completionTime) {
         console.error('completionTime is missing in the latest survey.');
         setDailySurveyCompleted(false);
+        setLoading(false);
         return;
       }
 
       const completionDate = new Date(latestSurvey.completionTime);
 
-      // Check if the conversion to a date was successful
       if (isNaN(completionDate.getTime())) {
         console.error('Invalid completionTime in the latest survey.');
         setDailySurveyCompleted(false);
+        setLoading(false);
         return;
       }
 
-      // Adjust the completion date to Greece time
       const today = adjustToGreeceTime(new Date());
 
-      // Check if the completion date and today are the same day
       if (areSameDay(completionDate, today)) {
         setDailySurveyCompleted(true);
       } else {
@@ -69,13 +73,23 @@ export const DailySurveyProvider = ({ children }) => {
     } catch (error) {
       console.error('An error occurred while checking the survey status:', error);
       setDailySurveyCompleted(false);
+    } finally {
+      setLoading(false);
     }
   };
 
-  // Effect to check survey status on component mount
   useEffect(() => {
+    console.log('authData in useEffect:', authData);
     checkDailySurvey();
   }, [authData]);
+
+  if (loading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
 
   return (
     <DailySurveyContext.Provider value={{ dailySurveyCompleted, setDailySurveyCompleted }}>
