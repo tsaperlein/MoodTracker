56fd315995447634685e753d4d2d1ed419ab3ac5
Alexandros Tsaparas <tsaperlein@gmail.com>
2024-09-16 22:56:07 +0300

    Add some new utilities

--- api/src/utils/datetime.mjs
+++ api/src/utils/datetime.mjs
@@ -6,3 +6,16 @@ export function adjustToGreeceTime(date) {
 export function areDatesOnDifferentDays(d1, d2) {
   return d1.getUTCDate() !== d2.getUTCDate();
 }
+
+export function getRandomTimeForDay(day) {
+  const start = new Date(day);
+  start.setHours(0, 0, 0, 0);
+
+  const end = new Date(day);
+  end.setHours(23, 59, 59, 999);
+
+  const randomTime = new Date(
+    start.getTime() + Math.random() * (end.getTime() - start.getTime())
+  );
+  return adjustToGreeceTime(randomTime); // Adjust to Greece time
+}
